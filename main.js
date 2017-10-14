1846c9393a6774266a626e275741770aa838844f
Sean Walsh <seanmichwalsh@gmail.com>
2017-10-14 18:07:18 -0400

    Michelle's buttons

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 function joinFunc() {
 	var name = prompt("Enter your name: ", "your name here");
-	document.write("You have entered: " + name);
+	/*document.write("You have entered: " + name);
+	*/
 }
 function askDetails() {
 	var eventName = prompt("Enter your event name: ");
 	var location = prompt("Enter your location: ");
 	var eventTime = prompt("Enter the time: ");
 	var capacity = prompt("Enter the capacity: ");
-	document.write("You have entered: " + eventName);
+	/*document.write("You have entered: " + eventName);
 	document.write("You have entered: " + location);
 	document.write("You have entered: " + eventTime);
 	document.write("You have entered: " + capacity);
+	*/
 }
